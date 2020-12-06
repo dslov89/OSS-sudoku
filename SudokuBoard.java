a1b365fffacb02150fa1a3ad4bb521ed5e94a133
Godohr <70680600+Godohr@users.noreply.github.com>
2020-12-06 20:30:31 +0900

    Update SudokuBoard.java
    fill 메소드 추가

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,6 +1,7 @@
 import PuzzleGame.PuzzlePiece;
 
 import java.util.Random;
+import java.util.SimpleTimeZone;
 
 public class SudokuBoard {
 
@@ -27,36 +28,23 @@ public class SudokuBoard {
 
     }
     /**정답보드 만들기*/  //모르겠음
-    public void rightBoard(){
-        for (int i = 1; i < 5; i++){
-            SudokuPiece p = new SudokuPiece(i);
-            right_board[0][i - 1] = p;
-        }
-        SudokuPiece p = new SudokuPiece(3);
-        right_board[1][0] = p;
-        SudokuPiece p1 = new SudokuPiece(4);
-        right_board[1][1] = p1;
-        SudokuPiece p2 = new SudokuPiece(1);
-        right_board[1][2] = p2;
-        SudokuPiece p3 = new SudokuPiece(2);
-        right_board[1][3] = p3;
-
-        SudokuPiece p4 = new SudokuPiece(2);
-        right_board[2][0] = p4;
-        SudokuPiece p5 = new SudokuPiece(1);
-        right_board[2][1] = p5;
-        SudokuPiece p6 = new SudokuPiece(4);
-        right_board[2][2] = p6;
-        SudokuPiece p7 = new SudokuPiece(3);
-        right_board[2][3] = p7;
-
-        for(int i = 1; i < 5; i++){
-            SudokuPiece p8 = new SudokuPiece(i);
-            right_board[3][4 - i] = p8;
+    public void rightBoard() {
+        for (int i = 0; i < SIZE; i++){
+            right_board[0][i] = new SudokuPiece(i);
         }
 
-        //줄 랜덤 바꾸기
+        for (int i = 0; i < SIZE; i++{
+            int a = (int)(Math.random() * SIZE);
+            SudokuPiece tmp = right_board[0][a];
+
+        }
     }
+
+
+    public void fill(int n, int row, int col){
+        board[row][col] = new SudokuPiece(n);
+    }
+
     public SudokuPiece[][] contents() {
         SudokuPiece[][] answer = new SudokuPiece[SIZE][SIZE];
         for (int i = 0; i != SIZE; i = i + 1)
@@ -65,3 +53,4 @@ public class SudokuBoard {
         return answer;
     }
 }
+
