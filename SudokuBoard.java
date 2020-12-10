42079e251d46f3289eb9af06332e386e244f3d5c
Godohr <70680600+Godohr@users.noreply.github.com>
2020-12-10 15:35:47 +0900

    Update SudokuBoard.java

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,4 +1,4 @@
-import PuzzleGame.PuzzlePieceimport java.util.Random;
+import java.util.Random;
 import java.util.SimpleTimeZone;
 
 public class SudokuBoard {
@@ -81,7 +81,25 @@ public class SudokuBoard {
         SudokuPiece[][] arr = new SudokuPiece[SIZE][SIZE];
 
         //줄 랜덤으로 바꾸기
+        for(int i = 0; i%2==0&&i<SIZE; i++){
+            if (Math.random() >= 0.5) {
+                for (int j = 0; j < SIZE; j++) {
+                    arr[i][j] = right_board[i][j];
+                    right_board[i][j].getValue(right_board[i + 1][j].valueOf());
+                    right_board[i + 1][j].getValue(arr[i][j].valueOf());
+                }
+            }
+        }
 
+        for(int i = 0; i%2==0&&i<SIZE; i++){
+            if (Math.random() >= 0.5) {
+                for (int j = 0; j < SIZE; j++) {
+                    arr[j][i] = right_board[j][i];
+                    right_board[j][i].getValue(right_board[j][i+1].valueOf());
+                    right_board[j][i+1].getValue(arr[j][i].valueOf());
+                }
+            }
+        }
 
     }
 
@@ -114,59 +132,3 @@ public class SudokuBoard {
         return answer;
     }
 }
-;
-
-import java.util.Random;
-import java.util.SimpleTimeZone;
-
-public class SudokuBoard {
-
-    private int SIZE = 4;
-    public SudokuPiece[][] board;
-    private SudokuPiece[][] right_board;
-    private int level;
-
-
-    public SudokuBoard(int l){
-        level = l;
-        board = new SudokuPiece[SIZE][SIZE];
-        right_board = new SudokuPiece[SIZE][SIZE];
-        rightBoard();
-
-        //난이도에 따라 랜덤으로 퍼즐보드 만들기
-        board = right_board;
-        board[0][3] = new SudokuPiece(0);
-        board[1][2] = new SudokuPiece(0);
-        board[3][3] = new SudokuPiece(0);
-        board[2][0] = new SudokuPiece(0);
-        board[1][0] = new SudokuPiece(0);
-        board[2][2] = new SudokuPiece(0);
-
-    }
-    /**정답보드 만들기*/  //모르겠음
-    public void rightBoard() {
-        for (int i = 0; i < SIZE; i++){
-            right_board[0][i] = new SudokuPiece(i);
-        }
-
-        for (int i = 0; i < SIZE; i++{
-            int a = (int)(Math.random() * SIZE);
-            SudokuPiece tmp = right_board[0][a];
-
-        }
-    }
-
-
-    public void fill(int n, int row, int col){
-        board[row][col] = new SudokuPiece(n);
-    }
-
-    public SudokuPiece[][] contents() {
-        SudokuPiece[][] answer = new SudokuPiece[SIZE][SIZE];
-        for (int i = 0; i != SIZE; i = i + 1)
-            for (int j = 0; j != SIZE; j = j + 1)
-                answer[i][j] = right_board[i][j];
-        return answer;
-    }
-}
-
